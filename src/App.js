93d35005a3ec24c91b41030b22406175540dea77
Rohaan Malik <36078297+rohaanmalik@users.noreply.github.com>
2021-08-17 19:11:27 +0000

    Added the eth wallet connect functionality + button

--- src/App.js
+++ src/App.js
@@ -7,17 +7,16 @@ export default function App() {
   const [currAccount, setCurrAccount] = React.useState("");
 
   const checkIfWalletIsConnected = () => {
-    const ethereum = window;
+    const { ethereum } = window;
     if (!ethereum){
       console.log("make sure you have metamask")
       return;
     } else {
       console.log(" We have an ethereum object", ethereum)
     }
-  }
 
-  // basically we are trying to get the account info
-  ethereum.request({ method: 'eth_accounts'})
+      // basically we are trying to get the account info
+    ethereum.request({ method: 'eth_accounts'})
           .then(accounts => {
             if (accounts.length !== 0){
               const account = accounts[0];
@@ -27,6 +26,20 @@ export default function App() {
               console.log("No authorised account found")
             }
           })
+  }
+
+  const connectWallet = () => {
+    const { ethereum } = window;
+    if (!ethereum){
+      alert("Get Metamask wallet")
+    }
+    ethereum.request({method: 'eth_requestAccounts'})
+    .then(accounts => {
+      console.log("ethereum wallet connected: ", accounts[0])
+      setCurrAccount(accounts[0])
+    })
+    .catch(err => console.log(err))
+  }
 
   React.useEffect(() => {
       checkIfWalletIsConnected();
@@ -51,6 +64,12 @@ export default function App() {
         <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
+
+        {currAccount ? null : (
+          <button classname="waveButton" onClick={connectWallet}>
+          Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
